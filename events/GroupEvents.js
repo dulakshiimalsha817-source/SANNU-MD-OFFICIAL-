81fb5175e5de9e6890edc0859e0326690fc49c24
dulakshiimalsha817-source <dulakshiimalsha817@gmail.com>
2025-11-16 11:39:34 +0530

    Update GroupEvents.js

--- events/GroupEvents.js
+++ events/GroupEvents.js
@@ -175,8 +175,10 @@ module.exports = async (conn, update, m) => {
 │ 📝 𝙶𝚁𝙾𝚄𝙿 𝙳𝙴𝚂𝙲𝚁𝚁𝙸𝙿𝚃𝙸𝙾𝙽:
 │ ${desc}
 │ 
-│ 🥷 ʙᴏᴛ ᴄʜᴀɴɴᴇʟ ꜰᴏʟʟᴏᴡ: https://whatsapp.com/channel/0029VbC2V7k3QxS4uRS8cB1P
-│ 🧚‍♂️ ʙᴏᴛ ɢʀᴏᴜᴘ ᴊᴏɪɴ: https://chat.whatsapp.com/LcOBCsUP0wl8xSDnngXzjp?mode=wwt
+│ 🥷 ʙᴏᴛ ᴄʜᴀɴɴᴇʟ ꜰᴏʟʟᴏᴡ:
+https://whatsapp.com/channel/0029VbC2V7k3QxS4uRS8cB1P
+│ 🧚‍♂️ ʙᴏᴛ ɢʀᴏᴜᴘ ᴊᴏɪɴ:
+https://chat.whatsapp.com/LcOBCsUP0wl8xSDnngXzjp?mode=wwt
 │
 │      © 𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈🥷𝚂𝙰𝙽𝙽𝚄 𝙼𝙳 𝙼𝙸𝙽𝙸 𝙱𝙾𝚃
 ╰─────────────-─────
@@ -201,8 +203,10 @@ module.exports = async (conn, update, m) => {
 │ 
 │ 🌟 𝐖𝙴 𝙻𝙻 𝐌𝙸𝚂𝚂 𝐘𝙾𝚄 👋
 │
-│ 🥷 ʙᴏᴛ ᴄʜᴀɴɴᴇʟ ꜰᴏʟʟᴏᴡ: https://whatsapp.com/channel/0029VbC2V7k3QxS4uRS8cB1P
-│ 🧚‍♂️ ʙᴏᴛ ɢʀᴏᴜᴘ ᴊᴏɪɴ: https://chat.whatsapp.com/LcOBCsUP0wl8xSDnngXzjp?mode=wwt
+│ 🥷 ʙᴏᴛ ᴄʜᴀɴɴᴇʟ ꜰᴏʟʟᴏᴡ:
+https://whatsapp.com/channel/0029VbC2V7k3QxS4uRS8cB1P
+│ 🧚‍♂️ ʙᴏᴛ ɢʀᴏᴜᴘ ᴊᴏɪɴ:
+https://chat.whatsapp.com/LcOBCsUP0wl8xSDnngXzjp?mode=wwt
 │
 │      © 𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈🥷𝚂𝙰𝙽𝙽𝚄 𝙼𝙳 𝙼𝙸𝙽𝙸 𝙱𝙾𝚃
 ╰─────────────-─────
